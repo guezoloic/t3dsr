189653268fb6f5136d4485f443d5f9308be8b654
Loïc GUEZO <loicguezo@gmail.com>
2025-03-09 19:55:28 +0100

    feat(test_vec3): added all unit tests for vector operations

--- test/test_main.c
+++ test/test_main.c
@@ -1,3 +1,15 @@
+#include "test_vec3.h"
+
+#include <stdio.h>
+
 int main() {
+    addVector3_test();
+    subVector3_test();
+    scaleVector3_test();
+    dotVector3_test();
+    lenVector3_test();
+    normVector3_test();
+    distVector3_test();
+    printf("All Vector4 tests succeeded\n");
     return 0;
 }
