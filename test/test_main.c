f367c7a9f6d3f454901729de25e119239f385a5b
Loïc GUEZO <loicguezo@gmail.com>
2025-03-15 20:15:54 +0100

    feat(test_vec): add cross product function test

--- test/test_main.c
+++ test/test_main.c
@@ -10,16 +10,17 @@ int main() {
     
     addVector3_test();
     subVector3_test();
-    scaleVector3_test();
+    scalVector3_test();
     dotVector3_test();
     lenVector3_test();
     normVector3_test();
     lerpVector3_test();
+    crosVector3_test();
     anglVector3_test();
     projVector3_test();
     reflVector3_test();
     distVector3_test();
-    rotateVector3_test();
+    rotaVector3_test();
     SUCCESS("Vector3");
 
     // --- Vector4 ---
