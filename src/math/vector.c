b613081224d84034cd24cf79d78c8b2e909bb8ef
Loïc GUEZO <loicguezo@gmail.com>
2025-03-21 08:03:18 +0100

    fix: remove all heap allocation for better performance in  vector functions.

--- src/math/vector.c
+++ src/math/vector.c
@@ -1,11 +1,11 @@
 #include "math/vector.h"
 
-Vec3* Vec4ToVec3(Vec4* v)
+Vec3 Vec4ToVec3(Vec4 v)
 {
-    return vec3(v->x, v->y, v->z);
+    return vec3(v.x, v.y, v.z);
 }
 
-Vec4* Vec3ToVec4(Vec3* v) 
+Vec4 Vec3ToVec4(Vec3 v) 
 {
-    return vec4(v->x, v->y, v->z, 0);
+    return vec4(v.x, v.y, v.z, 0);
 }
