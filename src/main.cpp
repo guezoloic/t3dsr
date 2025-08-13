a5a9cc596d30854295e010141b7cb09d96a3bc2f
Loïc GUEZO <loicguezo@gmail.com>
2025-08-13 09:37:52 +0200

    feat(mat4.hpp): add cpp port

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-
-extern "C"
-{
-    #include "math/mat4.h"
-    #include "math/vec3.h"
-    #include "math/vec4.h"
-}
+#include "math/mat4.hpp"
 
 int main()
 {
+    math::Mat4f test = math::Mat4f::Identity();
+    std::cout << test.data().m[0] << test.data().m[5] << test.data().m[10] << test.data().m[15] << std::endl;
     return 0;
 }
