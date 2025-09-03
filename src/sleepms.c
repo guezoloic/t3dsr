9ae586e0bae9b27f5289d9035354e4dcd0f3a4d7
Loïc GUEZO <loicguezo@gmail.com>
2025-09-03 20:38:30 +0200

    fix: remove posix segment

--- src/sleepms.c
+++ src/sleepms.c
@@ -2,8 +2,6 @@
 
 #ifdef WIN32
 #include <windows.h>
-#elif _POSIX_C_SOURCE >= 199309L
-#include <time.h>   // for nanosleep
 #else
 #include <unistd.h> // for usleep
 #endif
@@ -11,11 +9,6 @@
 void sleep_ms(int milliseconds){ // cross-platform sleep function
 #ifdef WIN32
     Sleep(milliseconds);
-#elif _POSIX_C_SOURCE >= 199309L
-    struct timespec ts;
-    ts.tv_sec = milliseconds / 1000;
-    ts.tv_nsec = (milliseconds % 1000) * 1000000;
-    nanosleep(&ts, NULL);
 #else
     if (milliseconds >= 1000)
       sleep(milliseconds / 1000);
